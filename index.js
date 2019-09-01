b3aa6d0d53adfaa429dd87347fad72f778a38305
Pavel Stepin <pvstepin@avito.ru>
2019-09-01 16:40:47 +0300

    старт

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send('Hello World');
 });
+
+console.log('START APP 4000');
 
 app.listen(3000);
 
